d5b72254302ad6c04858d61ae4b9026996d2be87
chalana kalpitha <chalanakalpitha72@gmail.com>
2022-09-17 22:12:16 +0530

    createuser routes and controller

--- server/index.js
+++ server/index.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 require("dotenv").config();
 
+app.use(cors());
+app.use(express.json());
 
+app.use("/api/auth", userRoutes);
 
-app.use(cors())
-app.use(express.json())
-
-
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("DB Connected Successfully");
-})
+  });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server run on port ${process.env.PORT}`);
-})
+  console.log(`Server run on port ${process.env.PORT}`);
+});
